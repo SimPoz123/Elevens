27ec6cd591e4c1708777ce782c76d580c10036d4
SimPoz123 <simwpoz@gmail.com>
2017-04-07 12:04:39 -0600

    Finished Questions

--- src/Shuffler.java
+++ src/Shuffler.java
@@ -9,7 +9,7 @@ public class Shuffler {
 	 * The number of consecutive shuffle steps to be performed in each call
 	 * to each sorting procedure.
 	 */
-	private static final int SHUFFLE_COUNT = 1;
+	private static final int SHUFFLE_COUNT = 3;
 
 
 	/**
@@ -19,7 +19,7 @@ public class Shuffler {
 	public static void main(String[] args) {
 		System.out.println("Results of " + SHUFFLE_COUNT +
 								 " consecutive perfect shuffles:");
-		int[] values1 = {0, 1, 2, 3};
+		int[] values1 = {0, 1, 2, 3, 4};
 		for (int j = 1; j <= SHUFFLE_COUNT; j++) {
 			perfectShuffle(values1);
 			System.out.print("  " + j + ":");
@@ -32,7 +32,7 @@ public class Shuffler {
 
 		System.out.println("Results of " + SHUFFLE_COUNT +
 								 " consecutive efficient selection shuffles:");
-		int[] values2 = {0, 1, 2, 3};
+		int[] values2 = {0, 1, 2, 3, 4, 5};
 		for (int j = 1; j <= SHUFFLE_COUNT; j++) {
 			selectionShuffle(values2);
 			System.out.print("  " + j + ":");
@@ -42,6 +42,26 @@ public class Shuffler {
 			System.out.println();
 		}
 		System.out.println();
+
+		System.out.println("Ten Flips:");
+		for (int i = 0; i < 10; i++) {
+			if (flip()) {
+				System.out.println("Heads");
+			} else {
+				System.out.println("Tails");
+			}
+		}
+
+		int[] values3 = {0, 1, 2, 3,};
+		int[] values4 = {2, 1, 3, 0};
+		int[] values5 = {2, 1, 5, 3};
+
+		if (arePermuations(values3, values4)) {
+			System.out.println("3 and 4 are permutations");
+		}
+		if (!(arePermuations(values3, values5))) {
+			System.out.println("3 and 5 are not permutations");
+		}
 	}
 
 
@@ -61,7 +81,7 @@ public class Shuffler {
 		for (int i = (values.length + 1) / 2; i < values.length; i = i + 2) {
 			shuffled[i] = values[i];
 		}
-
+		values = shuffled;
 	}
 
 	/**
@@ -78,12 +98,49 @@ public class Shuffler {
 	public static void selectionShuffle(int[] values) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 3 *** */
 		Random rand = new Random();
-		for (int k = 51; k > 0; k = k - 1) {
+		for (int k = values.length - 1; k > 0; k = k - 1) {
 			int r = rand.nextInt(k + 1);
 			int temp = values[r];
 			values[r] = values[k];
 			values[k] = temp;
 		}
+
+	}
+
+	public static boolean flip() {
+		Random rand = new Random();
+		int r = rand.nextInt(3);
+		boolean heads;
+		if (r == 2) {
+			heads = false;
+		} else {
+			heads = true;
+		}
+
+		return heads;
+	}
+
+	public static boolean arePermuations(int[] a, int[] b) {
+		int count = 0;
+		for (int i = 0; i < a.length; i++) {
+			boolean has = false;
+			for (int j = 0; j < b.length; j++) {
+				if (!(has)) {
+					if (a[i] == b[j]) {
+						has = true;
+					}
+				}
+			}
+			if (has) {
+				count = count + 1;
+			}
+		}
+
+		if (count == a.length) {
+			return true;
+		}
+
+		return false;
 
 	}
 }
